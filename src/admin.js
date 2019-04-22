707f08bca88af9972f877e6f71da5bff373338cb
xushijie <shijie10086@163.com>
2019-04-22 23:12:53 +0800

    完成页面架构
    header
    counter
    footer

--- src/admin.js
+++ src/admin.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Row, Col} from "antd";
+import Header from './components/Header'
+import Footer from './components/Footer';
 export default class Admin extends React.Component {
 
     render(){
         return (
             <div>
                 <Row>
-                    <Col span={6} >6</Col>
-                    <Col span={6} >6</Col>
-                    <Col span={6} >6</Col>
-                    <Col span={6} >6</Col>
-                    <Col span={6} >6</Col>
+                    <Col span={3} >
+                    left
+                    </Col>
+                    <Col span={21} >
+                        <Header>
+                            header
+                        </Header>
+                        <Row>
+                            counter
+                        </Row>
+                        <Footer>
+                            footer
+                        </Footer>
+                    </Col>
+                    
                 </Row>
                 
             </div>
