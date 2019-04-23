69d8cdadd58650cb5d1494774c5f037fb6b885f6
xushijie <shijie10086@163.com>
2019-04-24 01:12:23 +0800

    完成首页布局

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import "./index.less";
 export default class Header extends React.Component {
+    //#endregion
+    state = {
+        userName:'奔波儿灞'
+    }
+    componentDidMount() {
+        setInterval(() => {
+                new Date();
+        }, 1000);
+    }
 
     render(){
         return (
-            <div>
-                Header
+            <div className='header'>
+                <Row className='header-top'>
+                    <Col span={24} >
+                        <span>{this.state.userName},欢迎你</span>
+                        <a href='#'>退出</a>
+                    </Col>
+                </Row>
+
+                <Row className='breadcrumb'>
+                    <Col span={4} className='breadcrumb-title'>
+                        首页
+                    </Col>
+                    <Col span={20} className='weather'>
+                        <span>{'2019-04-20'}</span>
+                        <span className='weather-detail'>{'2019-04-20'}</span>
+                    </Col>
+                </Row>
+                
             </div>
         )
     }
