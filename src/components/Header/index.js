83f4bbafc28d1d3f5a7a9448cf0217e0c56ab8d6
xushijie <shijie10086@163.com>
2019-04-24 21:37:25 +0800

    新增jsonp插件，完成Header组件
    1  npm i jsonp
    2 封装jsonp
    3 jsonp的应用
    4 完成header组件的样式

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row, Col } from 'antd';
 import "./index.less";
 import utils from "../../utils/utils";
+import axios from '../../axios'
 export default class Header extends React.Component {
     //#endregion
     state = {
@@ -15,8 +16,23 @@ export default class Header extends React.Component {
                     sysTime
                 })
         }, 1000);
+        this.getWeatherApiData();
     }
 
+    getWeatherApiData = () => {
+        let city = '武汉';
+        axios.jsonp({
+            url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+        }).then((res)=>{
+            if(res.status === 'success'){
+                let data = res.results[0].weather_data[0];
+                this.setState({
+                    dayPictureUrl:data.dayPictureUrl,
+                    weather:data.weather
+                })
+            }
+        })
+    }
     render(){
         return (
             <div className='header'>
@@ -33,7 +49,12 @@ export default class Header extends React.Component {
                     </Col>
                     <Col span={20} className='weather'>
                         <span className='date'>{this.state.sysTime}</span>
-                        <span className='weather-detail'>{'晴天'}</span>
+                        <span className='weather-img'>
+                            <img src={this.state.dayPictureUrl} alt=''/>
+                        </span>
+                        <span className='weather-detail'>
+                            {this.state.weather}
+                        </span>
                     </Col>
                 </Row>
                 
---
+++ src/resource/doc.js
@@ -0,0 +1,2 @@
+// 百度的天气接口
+// http://api.map.baidu.com/telematics/v3/weather?location=武汉&output=json&ak=KGAONFnlm6oHGlyTQuuqhKd9z7DhHzFG
