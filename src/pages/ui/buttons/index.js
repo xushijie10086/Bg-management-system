c4e282d41a8926be8719208a7d2bd8c03c37b7b3
xushijie <shijie10086@163.com>
2019-05-08 21:58:25 +0800

    button组件的编写
    1 新增了很多buttons
    2 熟悉了button相关的属性
    3 了解了事件处理
    4 熟悉了Radio复选框的使用
    5 新增了路由内的组件 Switch 以精准匹配，避免穿透

--- src/pages/ui/buttons/index.js
+++ src/pages/ui/buttons/index.js
@@ -1,10 +1,69 @@
 import React from 'react';
+import { Card, Button, Radio } from 'antd';
+import './../ui.less'
 
 export default class Buttons extends React.Component {
+    state = {
+        loading: true
+    }
+    //#endregion
+    handleCloseLoading = () => {
+        this.setState({
+            loading: !this.state.loading,
+            size:'default'
+        })
+    }
     render () {
+        const { loading } = this.state;
         return (
-            <div>
-                this is buttons page!
+            <div className='buttons'>
+                <Card title="基础按钮" >
+                    <Button type='primary'>默认</Button>
+                    <Button>纯按钮</Button>
+                    <Button type='dashed'>dashed</Button>
+                    <Button type='danger'>danger</Button>
+                    <Button disabled>禁用</Button>
+                </Card>
+                
+                <Card title="图形按钮">
+                    <Button icon='plus'>创建</Button>
+                    <Button icon='edit'>编辑</Button>
+                    <Button icon='delete'>删除</Button>
+                    <Button shape='circle' icon='search'></Button>
+                    <Button type='primary' icon='search'>搜索</Button>
+                    <Button type='primary' icon='download'>下载</Button>
+                </Card>
+
+                <Card title="loading按钮">
+                    <Button type='primary' loading={loading}>确定</Button>
+                    <Button shape='circle' type='primary' loading={loading}></Button>
+                    <Button loading={loading}>点击加载</Button>
+                    <Button shape='circle' loading={loading}></Button>
+                    <Button type='primary' onClick={this.handleCloseLoading}>切换</Button>
+                </Card>
+
+                <Card title="按钮组">
+                   <Button.Group>
+                     <Button type='primary' icon='left' style={{marginRight:0}}>前进</Button>  
+                     <Button type='primary' icon='right'>返回</Button>  
+                   </Button.Group>
+                </Card>      
+
+                <Card title="按钮尺寸">
+                    <Radio.Group value={this.state.size} onChange={(e) => {
+                        this.setState({
+                            size: e.target.value
+                        })
+                    }}>
+                        <Radio value='small'>小</Radio>
+                        <Radio value='default'>中</Radio>
+                        <Radio value='large'>大</Radio>
+                    </Radio.Group>
+                    <Button type='primary' size={this.state.size}>奔波儿灞</Button>
+                    <Button type='dashed' size={this.state.size}>奔波儿灞</Button>
+                    <Button size={this.state.size}>奔波儿灞</Button>
+                    <Button type='danger' size={this.state.size}>奔波儿灞</Button>
+                </Card>
             </div>
         )
     }
--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/login";
 import Admin from "./admin";
-import Buttons from './pages/ui/buttons'
+import Buttons from "./pages/ui/buttons";
 import NoMatch from "./pages/nomatch";
 
 export default class ERouter extends React.Component {
@@ -12,20 +12,26 @@ export default class ERouter extends React.Component {
       // 定义路由的方式  为hash
       <HashRouter>
         {/* 加载了根组件 APP  根组件只需要接纳所有的同级大组件 { this.props.children }*/}
-        <App> 
+        <App>
+          <Switch>
             {/* login组件 与Admin 以及 后期的订单详情组件为同级组件 */}
             <Route path="/login" component={Login} />
             {/* Admin组件内嵌套子组件 */}
-            <Route path="/admin" render={() => 
-            <Admin> 
-                {/* 子路由 */}
-                <Route path="/admin/ui/buttons" component={Buttons} />
-                <Route  component={NoMatch} />
-            </Admin>}>
-              
-            </Route>
+            <Route
+              path="/admin"
+              render={() => (
+                <Admin>
+                  {/* 子路由 */}
+                  <Switch>
+                    <Route path="/admin/ui/buttons" component={Buttons} />
+                    <Route component={NoMatch} />
+                  </Switch>
+                </Admin>
+              )}
+            />
             <Route path="/order/detail" component={Login} />
-            <Route  component={NoMatch} />
+            <Route component={NoMatch} />
+          </Switch>
         </App>
       </HashRouter>
     );
